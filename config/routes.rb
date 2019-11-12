8788670d50d69c73b57d1ce36b276a300a3fdb63
Brandon Teich <brandonteich123@gmail.com>
2019-11-12 00:34:33 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
-leRails.application.routes.draw do
+Rails.application.routes.draw do
   
 <<<<<<< HEAD
   
 =======
 >>>>>>> fc518f99d6cf4e97a70bca77dae606e0b5353c58
-  get 'sessions/new'
+ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
